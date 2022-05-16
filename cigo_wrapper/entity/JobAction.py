33ab5511b7b904f899bcd16ec92cc28015a3155a
Zubair <zubair1714@gmail.com>
2022-05-16 16:45:22 +0500

    Refactored Job & Job Action Code

--- cigo_wrapper/entity/JobAction.py
+++ cigo_wrapper/entity/JobAction.py
@@ -39,42 +39,7 @@ class JobAction:
         action = cls(ref_id=response_dic['id'], action_type=response_dic['type'],
                      description=response_dic['description'])
 
-        for key in response_dic.keys():
-            if key == 'action_id' and response_dic[key] is not None:
-                action.action_id = response_dic[key]
-            elif key == 'status' and response_dic[key] is not None:
-                action.status = response_dic[key]
-            elif key == 'status_counters' and response_dic[key] is not None:
-                action.status_counters = response_dic[key]
-            elif key == 'value' and response_dic[key] is not None:
-                action.value = response_dic[key]
-            elif key == 'unit_weight' and response_dic[key] is not None:
-                action.unit_weight = response_dic[key]
-            elif key == 'total_weight' and response_dic[key] is not None:
-                action.total_weight = response_dic[key]
-            elif key == 'unit_volume' and response_dic[key] is not None:
-                action.unit_volume = response_dic[key]
-            elif key == 'total_volume' and response_dic[key] is not None:
-                action.total_volume = response_dic[key]
-            elif key == 'piece_count' and response_dic[key] is not None:
-                action.piece_count = response_dic[key]
-            elif key == 'piece_count_unit' and response_dic[key] is not None:
-                action.piece_count_unit = response_dic[key]
-            elif key == 'quantity' and response_dic[key] is not None:
-                action.quantity = response_dic[key]
-            elif key == 'quantity_unit' and response_dic[key] is not None:
-                action.quantity_unit = response_dic[key]
-            elif key == 'handle_time' and response_dic[key] is not None:
-                action.handle_time = response_dic[key]
-            elif key == 'stop_location_id' and response_dic[key] is not None:
-                action.stop_location_id = response_dic[key]
-            elif key == 'invoice_number' and response_dic[key] is not None:
-                action.invoice_number = response_dic[key]
-            elif key == 'external_reference_id' and response_dic[key] is not None:
-                action.external_reference_id = response_dic[key]
-            elif key == 'shipping_barcode' and response_dic[key] is not None:
-                action.shipping_barcode = response_dic[key]
-            elif key == 'create_datetime' and response_dic[key] is not None:
-                action.create_datetime = response_dic[key]
+        for key, value in response_dic.items():
+            setattr(action, key, value)
 
         return action
